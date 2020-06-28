7313b3bcee020dfb64622e3ff0ce7587013a2b4f
Victoria <55848341+victoriaparkhomenko885@users.noreply.github.com>
2020-06-28 17:46:07 +0300

    init

---
+++ turtle_task1.py
@@ -0,0 +1,31 @@
+import turtle
+
+brain = turtle.Turtle()
+
+brain.screen.bgcolor("coral")
+brain.speed(6)
+brain.pensize(2)
+
+def paint(space, color):
+   brain.fillcolor(color)
+   brain.begin_fill()
+   
+   for i in range(4):
+       brain.forward(space)
+       brain.left(90)
+
+   brain.end_fill()
+
+colors = ['yellow','red','lime', 'red']
+for iter in range (4):
+    paint(100,colors[iter])
+    brain.left(90)
+
+brain.up()
+brain.color("blue")
+brain.forward(200)
+brain.down()
+brain.left(90)
+brain.circle(200)
+
+input()
