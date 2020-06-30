d6cfa65c3d4045b799546692498f6055523432cd
Victoria <55848341+victoriaparkhomenko885@users.noreply.github.com>
2020-06-30 18:12:27 +0300

    init

---
+++ Var1_task2.py
@@ -0,0 +1,17 @@
+# logarithmic spiral
+
+import turtle
+import math
+
+const = 1.3
+
+counter = 1
+
+spiral = turtle.Turtle()
+spiral.speed(10)
+
+for i in range(12):
+    for j in range(40):    
+        spiral.right(math.log(300/3.14))
+        spiral.forward(counter)
+    counter *= const
